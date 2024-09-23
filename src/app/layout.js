4d8f1e83be56ce7c318bab22ebcb8eb534f5dbad
kapil singh shekhawat <kapil@pinblooms.com>
2024-09-23 10:32:10 +0530

    next js without darkmode provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-// import Providers from "./providers";
 import Providers from "@/app/providers";
-
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
 
@@ -18,13 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-        <Header/>
-        <Navbar/>
-        <SearchBox/>
-        {children}
-        </Providers>
-        </body>
+        {/* <Providers> */}
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
+        {/* </Providers> */}
+      </body>
     </html>
   );
 }
