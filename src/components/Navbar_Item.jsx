a725be938685c6b454413fd322b282cd556ba05f
kapil singh shekhawat <kapil@pinblooms.com>
2024-10-14 11:03:44 +0530

    static genration build error fixed

--- src/components/Navbar_Item.jsx
+++ src/components/Navbar_Item.jsx
@@ -1,20 +1,14 @@
-"use client";
 
 import Link from "next/link";
 import React from "react";
-import { useSearchParams } from "next/navigation";
 
-export default function Navbar_Item({ title, param }) {
-  const searchParams = useSearchParams();
-  const genre = searchParams.get("genre");
-
-
+export default function Navbar_Item({ title,param, searchParams}) {
 
   return (
     <div>
       <Link
         className={`hover:text-amber-500 ${
-          genre === param
+          searchParams === param
             ? " underline underline-offset-8 decoration-4 decoration-orange-400 rounded-lg"
             : ""
         }`}
